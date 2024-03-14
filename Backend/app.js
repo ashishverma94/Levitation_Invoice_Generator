5526720c1c1d1a18c97ce3ab7c38cad3bab9aca7
ashishverma94 <hariashish1@gmail.com>
2024-03-14 15:47:04 +0530

    done

--- Backend/app.js
+++ Backend/app.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(cors(  
   {
-    origin:["http://localhost:5173"],
+    origin:["https://levitation-invoice-generator.vercel.app/"],
     credentials: true
   }
 )); 
