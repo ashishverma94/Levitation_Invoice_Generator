4f2d9ee83ec241ac4d77cab8e248a9bb6417a211
ashishverma94 <hariashish1@gmail.com>
2024-03-14 15:56:29 +0530

    url 1

--- Frontend/src/server.js
+++ Frontend/src/server.js
@@ -1,5 +1,5 @@
 export const server_dev = "http://localhost:8000/api/v2";
 export const backend_url_dev = "http://localhost:8000/"
 
-export const server = "https://levitation-invoice-generator-backend.vercel.app/api/v2";
+export const server = "https://levitation-invoice-generator.vercel.app/api/v2";
 export const backend_url = "https://levitation-invoice-generator-backend.vercel.app/" ;
