210baab807d585ad30426eab6eb899c2e09b207e
ashishverma94 <hariashish1@gmail.com>
2024-03-14 15:25:44 +0530

    remove icons

--- Frontend/src/pages/SignupPage.jsx
+++ Frontend/src/pages/SignupPage.jsx
@@ -4,7 +4,7 @@ import { toast } from "react-toastify";
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
-import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
+// import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
 
 const SignupPage = () => {
   const navigate = useNavigate();
@@ -105,7 +105,8 @@ const SignupPage = () => {
                     onChange={(e) => setPassword(e.target.value)}
                     className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
                   />
-                  {visible ? (
+                  {/* {visible ? (
+                    
                     <AiOutlineEye
                       className="absolute right-2 top-2 cursor-pointer"
                       size={25}
@@ -117,7 +118,7 @@ const SignupPage = () => {
                       size={25}
                       onClick={() => setVisible(true)}
                     />
-                  )}
+                  )} */}
                 </div>
               </div>
 
