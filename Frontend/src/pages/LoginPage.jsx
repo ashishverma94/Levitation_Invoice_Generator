7133a117dd97597a6fe665f1acd01e3a4ae2113e
ashishverma94 <hariashish1@gmail.com>
2024-03-16 17:26:54 +0530

    secure password

--- Frontend/src/pages/LoginPage.jsx
+++ Frontend/src/pages/LoginPage.jsx
@@ -4,7 +4,7 @@ import { toast } from "react-toastify";
 import { useEffect,useState } from "react";
 import { useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
-// import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
+import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
 
 const LoginPage = () => {
   const navigate = useNavigate();
@@ -89,7 +89,7 @@ const LoginPage = () => {
                   onChange={(e) => setPassword(e.target.value)}
                   className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
                 />
-                {/* {visible ? (
+                 {visible ? (
                   <AiOutlineEye
                     className="absolute right-2 top-2 cursor-pointer"
                     size={25}
@@ -101,7 +101,7 @@ const LoginPage = () => {
                     size={25}
                     onClick={() => setVisible(true)}
                   />
-                )} */}
+                )} 
               </div>
             </div>
             <div className={`flex items-center justify-between`}></div>
