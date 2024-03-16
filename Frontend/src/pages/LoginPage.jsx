6209e2a3bf4ab69951514e83c172a97dc26b2819
ashishverma94 <hariashish1@gmail.com>
2024-03-16 19:39:52 +0530

    commit 7

--- Frontend/src/pages/LoginPage.jsx
+++ Frontend/src/pages/LoginPage.jsx
@@ -89,8 +89,8 @@ const LoginPage = () => {
                     value={password}
                     onChange={(e) => setPassword(e.target.value)}
                     className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-                  />
-                  {/* {visible ? (
+                   />
+                  {/*   {visible ? (
                     <AiOutlineEye
                       className="absolute right-2 top-2 cursor-pointer"
                       size={25}
